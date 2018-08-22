2b7db0821582f234fc56a5fe3c5b02e5f880b3f8
teamlead11 <automationlead11@gmail.com>
2018-08-22 09:24:15 +0530

    added test case2

---
+++ src/testgit/Testcase2.java
@@ -0,0 +1,10 @@
+package testgit;
+
+public class Testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my test case2");
+	}
+
+}
