b8420215141cf3beaf78f0d1714f90a5b11981cf
teamlead11 <automationlead11@gmail.com>
2018-08-22 08:47:13 +0530

    added framework class

---
+++ src/testgit/Framework.java
@@ -0,0 +1,11 @@
+package testgit;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is my framework");
+	}
+
+}
